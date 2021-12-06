67517a73003fbe2d801b0d6337d41149d07c7fab
Waleed Jubeh <wjubeh@sadasol.com>
2021-12-06 21:49:03 +0200

    import search page in app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClient, HttpClientModule} from '@angular/common/http';
-import { SearchPageComponent } from './pages/search-page/search-page.component'
+import { SearchPageModule } from './pages/search-page/search-page.module';
 @NgModule({
   declarations: [
-    AppComponent,
-    SearchPageComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    HttpClientModule
+    HttpClientModule,
+    SearchPageModule
   ],
   providers: [HttpClient],
   bootstrap: [AppComponent]
--- src/app/pages/search-page/search-page.module.ts
+++ src/app/pages/search-page/search-page.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SearchPageComponent } from './search-page.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [SearchPageComponent],
   imports: [
     CommonModule
   ]
